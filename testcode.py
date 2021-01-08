7ffae8acd053c54c3fc92e6e7d44e8646e4d2349
normalfujen <chenw@cumtb.edu.cn>
2021-01-08 14:32:43 +0800

    test code for pytorch

---
+++ testcode.py
@@ -0,0 +1,15 @@
+# This is a sample Python pytorch script.
+import torch
+import numpy as np
+from PIL image Image
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    a = torch.Tensor(3,4,5,8)
+    print(a.shape)
+
+    c = np.random.rand(1)
+    b = torch.Tensor(c)
+    print(b)
+
